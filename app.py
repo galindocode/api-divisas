b1fd72c7ecb620de2d133bf9b8fdca5c24d5b4ad
Leonardo Galindo <galindocode@gmail.com>
2023-01-05 23:36:19 -0400

    refactor(bcv): :recycle: join methods of extract currency in a single method

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ PREFIX = '/api/v1'
 @app.route(f'{PREFIX}/bcv')
 def get_bcv_all():
     return response.success(data=bcv.get_all())
+
+
+@app.route(f'{PREFIX}/bcv/<string:currency>')
+def get_currency(currency):
+    all_currency = bcv.get_all()
+    if currency not in all_currency:
+        response.error(
+            message=f'Currency {currency} not found', status_code=404)
+
+    return response.success(data={currency: all_currency[currency]})
--- src/currency/bcv.py
+++ src/currency/bcv.py
@@ -3,33 +3,18 @@ from src.scraping import html as scraping
 BCV_URL = 'https://www.bcv.org.ve/'
 
 
-def extract_usd(html):
-    return html.find('div', {'id': 'dolar'}).find('strong').text
-
-
-def extract_eur(html):
-    return html.find('div', {'id': 'euro'}).find('strong').text
-
-
-def extract_cny(html):
-    return html.find('div', {'id': 'yuan'}).find('strong').text
-
-
-def extract_try(html):
-    return html.find('div', {'id': 'lira'}).find('strong').text
-
-
-def extract_rub(html):
-    return html.find('div', {'id': 'rublo'}).find('strong').text
+def extract_currency(html, currency):
+    text = html.find('div', {'id': currency}).find('strong').text
+    return text.strip()
 
 
 def get_all():
     html = scraping.get(BCV_URL)
     return {
-        "usd": extract_usd(html),
-        "eur": extract_eur(html),
-        "cyn": extract_cny(html),
-        "try": extract_try(html),
-        "rub": extract_rub(html),
+        "usd": extract_currency(html, 'dolar'),
+        "eur": extract_currency(html, 'euro'),
+        "cyn": extract_currency(html, 'yuan'),
+        "try": extract_currency(html, 'lira'),
+        "rub": extract_currency(html, 'rublo'),
 
     }
